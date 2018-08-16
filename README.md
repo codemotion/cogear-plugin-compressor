6bc995aa029530508ebf148bfa4f5bde15e34d32
Dmitriy Beliaev <admin@cogear.ru>
2018-08-16 21:38:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cogear.JS plugin **cogear-plugin-compressor**
+
+Add `compressor-webpack-plugin` in production mode.
+
+# Installation
+
+Go to the site folder and install plugin with command:
+```bash
+yarn add cogear-plugin-cogear-plugin-compressor
+# or 
+npm install cogear-plugin-cogear-plugin-compressor
+```
+
+Plugin will loads up automatically.
---
+++ plugin.js
@@ -0,0 +1,14 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
+module.exports = {
+		apply(cogear){
+			// Process hooks here
+			cogear.hooks.loadPagesForWebpack.tap('cogear-plugin-compressor',()=>{
+				cogear.webpackConfig.plugins.push(
+					new CompressionPlugin({
+						test: /\.(html|js|css|svg)$/
+					})
+				)
+			})
+		}
+}
